4c88109d715b9f74c8294595a00c8e39373b578e
teun816 <teunvandenouden106@gmail.com>
2026-09-10 12:56:55 +0200

    ja

--- index.php
+++ index.php
@@ -39,7 +39,7 @@
         <section class="content-section">
 
             <div class="text-content">
-                <h1>hoi</h1>
+                <h1></h1>
 
                 <p>
                 </p>
